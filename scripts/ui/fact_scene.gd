957b29dbbec7b7a3f1f6c634b8292027f4a1a7b5
Kathleen Povadora <60345682+kaeri-gg@users.noreply.github.com>
2026-05-15 03:10:22 +0800

    Add comic assets and integrate into fact scenes
    Add multiple comic panel images and their Godot import metadata, add a reusable comic UI scene (scenes/ui/comic.tscn) and controller script (scripts/ui/comic.gd), and integrate those comics into fact_scene.tscn (replace previous background usage with comic instances and new textures). Rename and reorganize several level background files (Level X Background.png -> Background X.png), add new background parts, and remove old level background files. Also minor UI tweaks (settings_button focus_mode) and updates to fact scene resources and scripts to support the new comic flow.

--- scripts/ui/fact_scene.gd
+++ scripts/ui/fact_scene.gd
@@ -4,9 +4,42 @@ extends Control
 @export_file("*.tscn") var next_scene_path: String = ""
 @export var fade_duration: float = UiConstants.DEFAULT_FADE_DURATION
 
+var _comics: Array[Comic] = []
+var _next_screen: ActionButton
+
 func _ready() -> void:
 	utils.fade_from_overlay(fade_duration)
 	_wire_button("NextScreen", next_scene_path)
+	_next_screen = get_node_or_null("%NextScreen")
+	if _next_screen:
+		_next_screen.visible = false
+	_start_comic_sequence()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		for comic in _comics:
+			if not comic.visible:
+				comic.fade_in()
+				_check_sequence_complete()
+				break
+
+func _start_comic_sequence() -> void:
+	for child in get_children():
+		if child is Comic:
+			_comics.append(child)
+	_comics.sort_custom(func(a: Comic, b: Comic) -> bool: return a.sequence < b.sequence)
+	for comic in _comics:
+		get_tree().create_timer(comic.duration).timeout.connect(func() -> void:
+			comic.fade_in()
+			_check_sequence_complete()
+		)
+
+func _check_sequence_complete() -> void:
+	for comic in _comics:
+		if not comic.visible:
+			return
+	if _next_screen:
+		_next_screen.visible = true
 
 func _wire_button(unique_name: String, scene_path: String) -> void:
 	var button: ActionButton = get_node_or_null("%" + unique_name)
